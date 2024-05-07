f0824291515822bbb5111d1d59f51d148bf4d02a
Deepak Kumaravelu <deepak1122003kumar@gmail.com>
2024-05-07 19:50:27 +0530

    camera access removed

--- torchlight.js
+++ torchlight.js
@@ -5,27 +5,30 @@ if (SUPPORTS_MEDIA_DEVICES) {
   //Get the environment camera (usually the second one)
   navigator.mediaDevices.enumerateDevices().then(devices => {
   
-    const cameras = devices.filter((device) => device.kind === 'videoinput');
+    // const cameras = devices.filter((device) => device.kind === 'videoinput');
 
-    if (cameras.length === 0) {
-      throw 'No camera found on this device.';
-    }
-    const camera = cameras[cameras.length - 1];
+    // if (cameras.length === 0) {
+    //   throw 'No camera found on this device.';
+    // }
+    // const camera = cameras[cameras.length - 1];
 
     // Create stream and get video track
     navigator.mediaDevices.getUserMedia({
-      video: {
-        deviceId: camera.deviceId,
-        facingMode: ['user', 'environment'],
-        height: {ideal: 1080},
-        width: {ideal: 1920}
-      }
+      // video: {
+      //   deviceId: camera.deviceId,
+      //   facingMode: ['user', 'environment'],
+      //   height: {ideal: 1080},
+      //   width: {ideal: 1920}
+      // }
+      video:false,
+      audio:false,
+      advanced:[{torch:true}]
     }).then(stream => {
       const track = stream.getVideoTracks()[0];
 
       //Create image capture object and get camera capabilities
-      const imageCapture = new ImageCapture(track)
-      const photoCapabilities = imageCapture.getPhotoCapabilities().then(() => {
+      // const imageCapture = new ImageCapture(track)
+      // const photoCapabilities = imageCapture.getPhotoCapabilities().then(() => {
 
         //todo: check if camera has a torch
 
@@ -46,7 +49,6 @@ if (SUPPORTS_MEDIA_DEVICES) {
         });
       });
     });
-  });
+  };
   
   //The light will be on as long the track exists
-}
